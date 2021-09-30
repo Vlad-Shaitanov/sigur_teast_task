f43d39640d0ec54a60e2f7e72e42979d15271da0
Vladislav <kelsenelenelvial@mail.ru>
2021-09-30 13:37:31 +0300

    Start working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,67 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, submitCount },
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = (data) => {
+    console.log("Отправлено", data);
+  };
+
+  const nameField = watch("name");
+  const ageField = watch("age");
+
+  // console.log("nameField", nameField);
+  // console.log("ageField", ageField);
+
+  console.log("error", errors);
+  let errorsStatus = !!Object.keys(errors).length;
+  console.log("errorOBJ", errorsStatus);
+
   return (
     <div className="App">
-      <div>Start project</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Форма</h1>
+        <label htmlFor="surname" style={{ display: "block" }}>
+          Фамилия
+        </label>
+        <input
+          type="text"
+          defaultValue="John"
+          id="name"
+          {...register("name", {
+            required: true,
+            maxLength: 10,
+          })}
+        />
+        {errors.name && <i>Обязательное поле. Не более 15 символов</i>}
+        <input
+          type="text"
+          {...register("age", {
+            required: true,
+            pattern: /\d?\d/,
+            min: 18,
+            max: 99,
+          })}
+          style={{ display: "block" }}
+        />
+        {errors.age && <i>Обязательное числовое поле. От 18 до 99</i>}
+        <select style={{ display: "block" }} {...register("answer")}>
+          <option value="yes">Да</option>
+          <option value="no">Нет</option>
+        </select>
+        <input
+          type="submit"
+          disabled={errorsStatus}
+          style={{ display: "block" }}
+        />
+        Форма отправлена {submitCount} раз.
+      </form>
     </div>
   );
 }
