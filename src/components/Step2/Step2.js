e6026fab6269c6cdc8565e267d57ced2af058351
Vladislav <kelsenelenelvial@mail.ru>
2021-10-08 22:58:02 +0300

    Rendering of workplaces in Step2

--- src/components/Step2/Step2.js
+++ src/components/Step2/Step2.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import WorkIcon from "@mui/icons-material/Work";
 import Typography from "@mui/material/Typography";
 import { MainContainer } from "../MainContainer/MainContainer";
@@ -14,10 +14,14 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 const scheme = yup.object().shape({});
 
 export const Step2 = () => {
+  //Места работы
+  const [works, setWorks] = useState([]);
+
   // const history = useHistory();
   const {
     register,
@@ -26,9 +30,17 @@ export const Step2 = () => {
     watch,
     formState: { errors },
   } = useForm({
+    defaultValues: {
+      untilNow: true,
+    },
     mode: "onBlur", //Режим валидации(по событию)
     resolver: yupResolver(scheme), //Запуск внешнего метода проверки
   });
+
+  const handleWorkplace = () => {
+    setWorks((prevState) => [...prevState, "new place"]);
+    console.log(works);
+  };
 
   const onSubmit = (data) => {
     console.log("DATA", data);
@@ -72,8 +84,35 @@ export const Step2 = () => {
                 />
               </RadioGroup>
             </FormControl>
+            {/*{experience && (*/}
+            {/*<Workplace*/}
+            {/*control={control}*/}
+            {/*error={errors}*/}
+            {/*watch={watch}*/}
+            {/*register={register}*/}
+            {/*/>*/}
+            {/*)}*/}
             {experience && (
-              <Workplace control={control} error={errors} watch={watch} />
+              <>
+                <p style={{ textAlign: "center" }}>Места работы:</p>
+                {works.map((place, indx) => (
+                  <Workplace
+                    control={control}
+                    error={errors}
+                    watch={watch}
+                    register={register}
+                    index={indx}
+                    key={indx}
+                  />
+                ))}
+                <Button
+                  size="medium"
+                  variant="contained"
+                  onClick={handleWorkplace}
+                >
+                  Добавить место
+                </Button>
+              </>
             )}
           </Box>
         </Box>
--- src/components/Workplace/Workplace.js
+++ src/components/Workplace/Workplace.js
@@ -5,81 +5,72 @@ import { ViewDatePicker } from "../DatePicker/DatePicker";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel/FormControlLabel";
 
-export const Workplace = ({ control, name, error, watch }) => {
-  const [checked, setChecked] = React.useState(true);
-
-  const handleChange = (event) => {
-    setChecked(event.target.checked);
-  };
-
+export const Workplace = ({ control, name, error, watch, register, index }) => {
   let untilNow = watch("untilNow");
-  console.log(untilNow);
-
+  // console.log("untilNow", untilNow);
+  console.log("INDEX", index);
   return (
     <>
-      <p style={{ textAlign: "center" }}>Места работы</p>
       <Box
         style={{
           border: "1px solid #DCDCDC",
           paddingRight: 4,
           paddingLeft: 4,
           borderRadius: "5px",
+          marginBottom: "20px",
         }}
       >
         <ViewDatePicker
           control={control}
-          id="startWork"
-          name="startWork"
+          id={`startWork${index}`}
+          name={`startWork${index}`}
           label="Начало работы"
         />
         <FormControlLabel
           control={
             <Checkbox
-              control={control}
-              color="primary"
+              {...register("untilNow")}
               name="untilNow"
-              checked={checked}
-              onChange={handleChange}
-              inputProps={{ "aria-label": "controlled" }}
+              color="primary"
+              defaultChecked={true}
             />
           }
           label="По настоящее время"
         />
-        {!untilNow && <div>Hello</div>}
-        {/*<Checkbox*/}
-        {/*// control={control}*/}
-        {/*color="primary"*/}
-        {/*name="untilNow"*/}
-        {/*label="По настоящее время"*/}
-        {/*checked={checked}*/}
-        {/*onChange={handleChange}*/}
-        {/*inputProps={{ "aria-label": "controlled" }}*/}
-        {/*/>*/}
+        {!untilNow && (
+          <ViewDatePicker
+            control={control}
+            id="endWork"
+            name="endWork"
+            label="Окончание работы"
+          />
+        )}
+
         <Input
           control={control}
-          id="company"
+          id={`company${index}`}
           type="text"
           label="Название компании"
-          name="company"
+          name={`company${index}`}
           size="small"
           error={error}
         />
         <Input
           control={control}
-          id="workPosition"
+          id={`workPosition${index}`}
           type="text"
           label="Должность"
-          name="workPosition"
+          name={`workPosition${index}`}
           size="small"
           error={error}
         />
         <Input
           control={control}
           error={error}
-          id="responsibilities"
+          id={`responsibilities${index}`}
           label="Обязанности"
           type="text"
-          name="responsibilities"
+          name={`responsibilities${index}`}
           multiline={true}
           maxRows={4}
           placeholder="Опишите Ваши типовые обязанности на прошлом месте"
